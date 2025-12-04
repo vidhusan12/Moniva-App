fd498896c72aa6fc1a26b8e9d8b45815329c5f4e
vidhusan12 <vidhusan709@gmail.com>
2025-12-05 00:50:13 +1300

    feat: redesigned bill section UI and added '+' button to navigate to bill form

--- app/(tabs)/bill.tsx
+++ app/(tabs)/bill.tsx
@@ -1,77 +1,37 @@
-import { addBilll, Bill, deleteBill, fetchAllBill } from "@/services/bill";
-import { useFocusEffect } from "expo-router";
+import { Bill, deleteBill, fetchAllBill } from "@/services/bill";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { router, useFocusEffect } from "expo-router";
 import React, { useState } from "react";
-import {
-  Alert,
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Alert, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { formatMongoDate } from "../../utils/mongoDate";
 
 const BillDetails = () => {
-  const [amount, setAmount] = useState("");
-  const [description, setDescription] = useState("");
-  const [frequency, setFrequency] = useState("");
-  const [startDate, setStartDate] = useState("")
   const [bills, setBills] = useState<Bill[]>([]);
   const [loading, setLoading] = useState(false);
-
-  const frequencyOptions = ["Weekly", "Fortnightly", "Monthly", "One Time"];
-
-  useFocusEffect(
-    React.useCallback(() => {
-      const loadBills = async () => {
-        try {
-          setLoading(true);
-          const data = await fetchAllBill();
-          setBills(data);
-        } catch (error) {
-          console.log("Error", error);
-        } finally {
-          setLoading(false);
-        }
-      };
-      loadBills();
-    }, [])
-  );
-
-  const handleSubmit = async () => {
-    const parsedAmount = parseFloat(amount);
-    if (
-      !parsedAmount ||
-      isNaN(parsedAmount) ||
-      description.trim() === "" ||
-      !frequency
-    ) {
-      Alert.alert("Error", "Enter amount, description, and select frequency.");
-      return;
-    }
-
+  // You will need this function to refresh the list when you come back
+  const loadBills = React.useCallback(async () => {
     try {
-      await addBilll({
-        amount: parsedAmount,
-        description: description.trim(),
-        frequency: frequency,
-        startDate: startDate,
-      });
-
-      Alert.alert("Success", "Bill added!");
-      setAmount("");
-      setDescription("");
-      setFrequency("");
+      setLoading(true);
       const data = await fetchAllBill();
       setBills(data);
     } catch (error) {
-      Alert.alert(
-        "Error",
-        error instanceof Error ? error.message : "Unknown error"
-      );
+    } finally {
+      setLoading(false);
     }
+  }, []); // Dependecies array is empty because no external states are needed
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadBills();
+
+      return () => {};
+    }, [loadBills])
+  );
+
+  const formatDateForDisplay = (date: Date) => {
+    return date.toLocaleDateString("en-GB");
   };
 
   const handleDelete = async (id?: string) => {
@@ -99,86 +59,18 @@ const BillDetails = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-[#ffffff]">
+      {/* Header adn Add Button */}
+      <View className="px-5 pt-8 flex-row justify-between items-center">
+        <Text className="text-2xl font-rubik-semibold">Your Bills</Text>
+        <TouchableOpacity
+          onPress={() => router.push("/newBill")}
+          className="p-2"
+        >
+          <Ionicons name="add-circle" size={32} color="#ffd33d"/>
+        </TouchableOpacity>
+      </View>
+
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        {/* Header */}
-        <View className="px-5 pt-8 items-start">
-          <Text className="text-2xl font-rubik-semibold">New Bill</Text>
-          <Text className="text-sm font-rubik-light text-gray-700 mb-4">
-            Add your bill and how often you receive it
-          </Text>
-        </View>
-
-        {/* Bill Amount Card */}
-        <View className=" px-5 items-start w-full">
-          <View className="w-full max-w-md bg-white rounded-2xl p-5 shadow-md shadow-black/10">
-            <Text className="font-rubik pb-4 text-lg tracking-wider">
-              Bill amount
-            </Text>
-            <TextInput
-              className="text-3xl font-rubik bg-transparent mb-8"
-              placeholder="$0.00"
-              value={amount}
-              onChangeText={setAmount}
-              keyboardType="numeric"
-              placeholderTextColor="#333"
-            />
-          </View>
-        </View>
-
-        {/* Description label OUTSIDE the box */}
-        <View className="items-start w-full px-5 mt-4">
-          <Text className="text-lg font-rubik mb-1">Description</Text>
-        </View>
-        <View className="items-start w-full">
-          <View className="w-11/12 max-w-md bg-white rounded-2xl mx-4 shadow-md shadow-black/10 ">
-            <TextInput
-              placeholder="e.g. Netflix, Prime"
-              value={description}
-              onChangeText={setDescription}
-              placeholderTextColor="#adb5bd"
-              className="text-base font-rubik bg-transparent py-4 px-3 min-h-[48px]"
-            />
-          </View>
-        </View>
-
-        {/* Frequency box */}
-        <View className="items-start w-full px-5 mt-4">
-          <Text className="text-lg font-rubik mb-1">Frequency</Text>
-          <View className="flex-row space-x-4">
-            {frequencyOptions.map((option) => (
-              <TouchableOpacity
-                key={option}
-                onPress={() => setFrequency(option)}
-                className={`px-4 py-2 rounded-xl border ${
-                  frequency === option
-                    ? "bg-[#ffd33d] border-[#ffd33d] text-black font-rubik-semibold"
-                    : "bg-white border-gray-300 text-gray-700 font-rubik"
-                }`}
-              >
-                <Text
-                  className={`${
-                    frequency === option
-                      ? "text-black font-rubik-semibold"
-                      : "text-gray-700 font-rubik"
-                  }`}
-                >
-                  {option}
-                </Text>
-              </TouchableOpacity>
-            ))}
-          </View>
-        </View>
-
-        {/* ADD & Cancel box */}
-        <View className="justify-center items-center gap-4  mt-4">
-          <TouchableOpacity
-            className="bg-blue-500 px-8 py-3 rounded-lg w-48"
-            onPress={handleSubmit}
-          >
-            <Text className="font-rubik text-lg text-center">Add</Text>
-          </TouchableOpacity>
-        </View>
-
         {/* Income details card */}
         <View className="items-start w-full px-5 mt-4 mb-6">
           <Text className="text-lg font-rubik mb-2">Your Bill</Text>
@@ -222,7 +114,7 @@ const BillDetails = () => {
                     •{" "}
                   </Text>
                   <Text className="font-rubik text-sm text-gray-600">
-                    Next pay: 30 Aug
+                    Due Date: {formatMongoDate(bill.startDate || "")}
                   </Text>
                 </View>
               </View>
